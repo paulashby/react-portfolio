8a5f83c75c2220702687b0d7e77114d6bb9ace7e
Paul Ashby <paul@primitive.co>
2023-02-06 17:45:41 +0000

    Add projectGallery with outbound links in descriptions

--- src/components/pages/ProjectGallery/index.js
+++ src/components/pages/ProjectGallery/index.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
+import Project from "../../Project";
+import projects from "../../../data/projects.json";
 
-function ProjectGallery() {
-  return (
-    <div>
-      <h1>Project Gallery Placeholder</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
-    </div>
-  );
+class ProjectGallery extends Component {
+  // Setting this.state.projects to the projects json array
+  state = {
+    projects
+  };
+
+  viewProject = id => {
+    this.state.projects.map(project => project.feature(project.id === id))
+  };
+
+  render() {
+    return (
+      <main className="container pl-5 pr-5">
+
+        <section id="work" className="row mb-5">
+    
+          <div id="work-intro" className="col-xs-12 col-sm-12 col-lg-4 offset-lg-4 text-center text-lg-left">
+            <h2>Featured Projects</h2>
+            <p>This is a selection of projects I&rsquo;ve worked on as sole developer - both websites and HTML5 games. Lorem
+              ipsum dolor sit amet consectetur, adipisicing elit. Earum eius sunt, fugit harum rem necessitatibus blanditiis
+            </p>
+          </div>
+    
+          <div className="work-samples row">
+
+          {/* Render projects */}
+            {this.state.projects.map(project => (
+              <Project
+                key={project.id}
+                title={project.title}
+                descriptor={project.descriptor}
+                inlineLinks={project.inlineLinks}
+                teaserImage={project.images.teaser}
+                description={project.description}
+                role={project.role}
+                deployedLink={project.deployedLink}
+              />
+            ))}
+
+          </div>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default ProjectGallery;
